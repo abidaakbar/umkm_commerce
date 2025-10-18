0d57045507295ecaf28a69d0b87daef2276ce140
Abida Akbar Rusyadin <111777607+abidaakbar@users.noreply.github.com>
2025-10-19 03:43:23 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application's home page.
-     *
-     * @return \Illuminate\View\View
-     */
+    
     public function index()
     {
-        // For now, this just shows a static welcome page.
-        // Later, you could pass featured products to the view.
         return view('home');
     }
 }
