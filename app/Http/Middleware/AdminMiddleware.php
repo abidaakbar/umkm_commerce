39f261993bf75f0002e4c75e48a12360c43918f1
Abida Akbar Rusyadin <111777607+abidaakbar@users.noreply.github.com>
2025-10-19 03:46:49 +0700

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle($request, Closure $next)
     {
         if (auth()->check() && auth()->user()->role === 'admin') {
